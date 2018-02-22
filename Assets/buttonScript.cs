9f51ded766a2956a4a2698d1913f7bd44ecee982
Massa Elias TTK17SP <eliasmassa@kamk.fi>
2018-02-22 19:07:55 +0200

    movement fix, rescaling level, adding button technology

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour
+{
+    public float gravity = 1;
+    public float xPosition = 0;
+    public float minHeight = 0;
+    public float maxHeight = 0;
+    private bool leftTriggerActive = false;
+    private bool rightTriggerActive = false;
+    
+    public string doorname;
+
+    void Update()
+    {
+        transform.position = new Vector2(xPosition, Mathf.Clamp(transform.position.y, minHeight, maxHeight));
+
+      
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (doorname != "Door2")
+        {
+            GetComponent<Rigidbody2D>().gravityScale = gravity;
+            GameObject.FindGameObjectWithTag(doorname).GetComponent<Rigidbody2D>().gravityScale = -0.5f;
+
+            Destroy(gameObject);
+        }
+        else if (doorname == "Door2")
+        {
+            if (other.CompareTag("Trigger"))
+            {
+                Debug.Log("left");
+                leftTriggerActive = true;
+            }
+            if (other.CompareTag("Trigger2"))
+            {
+                Debug.Log("right");
+                rightTriggerActive = true;
+            }
+
+            DoorCheck();
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Trigger"))
+        {
+            leftTriggerActive = false;
+        }
+        if (col.CompareTag("Trigger2"))
+        {
+            rightTriggerActive = false;
+        }
+    }
+
+    void DoorCheck()
+    {
+        if (leftTriggerActive == true && rightTriggerActive == true)
+        {
+            Debug.Log("kikkel");
+            GameObject.FindGameObjectWithTag(doorname).GetComponent<Rigidbody2D>().gravityScale = -0.5f;
+        }
+    }
+
+
+}
--- Assets/sizeScript.cs
+++ Assets/sizeScript.cs
@@ -4,35 +4,46 @@ using UnityEngine;
 
 public class sizeScript : MonoBehaviour
 {
-    public float xScale = 1;
-    public float yScale = 1;
-    public float yPosition;
+    private float xScale = 1;
+    private float yScale = 1;
     
-    private float maxScale = 3;
-    private float maxWidthScale = 3;
-    private Vector2 direction;
-    private int speed = 5;
+    
+    private float maxScale = 2.95f;
+    private float maxWidthScale = 2.95f;
+ 
+    private Rigidbody2D rb2d;
+    public float maxSpeed = 5f;
+    private float moveForce = 300f;
 
-    void Start()
+
+    void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     
     void FixedUpdate()
     {
-        //GetComponent<Rigidbody2D>().velocity = transform.forward * Input.GetAxis("HorizontalMove") * 100 * Time.deltaTime;
-        direction = Vector2.right;
-        direction = new Vector2(Input.GetAxis("HorizontalMove") * 100 * Time.deltaTime, 0);
-        direction.Normalize();
-        transform.Translate(direction * Time.deltaTime * speed);
 
 
+        float h = Input.GetAxis("HorizontalMove");
+        
+
+        if (h * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * h * moveForce);
+        }
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        }
+   
         HeightCheck();
         WidthCheck();
 
 
-        yPosition = GameObject.FindGameObjectWithTag("Player").transform.position.y;
+        
 
         transform.localScale = new Vector3(xScale, yScale, 1);
 
@@ -44,12 +55,12 @@ public class sizeScript : MonoBehaviour
 
         if (Input.GetAxisRaw("Horizontal") == 0)
         {
-            xScale = 1;
+            xScale = 0.95f;
         }
 
         if (Input.GetAxisRaw("Vertical") == 0)
         {
-            yScale = 1f;
+            yScale = 0.95f;
 
         }
 
@@ -61,7 +72,7 @@ public class sizeScript : MonoBehaviour
 
         if (Input.GetAxisRaw("Vertical") > 0 && yScale <= maxScale)
         {
-            Mathf.Clamp(yScale, 1f, 3f);
+            Mathf.Clamp(yScale, 0.95f, 2.95f);
             yScale += 0.1f;
             
         }
@@ -71,6 +82,11 @@ public class sizeScript : MonoBehaviour
         if (yScale > maxScale)
         {
             yScale = maxScale;
+        }
+
+        if (xScale > maxWidthScale)
+        {
+            xScale = maxWidthScale;
         }
 
         
@@ -90,59 +106,69 @@ public class sizeScript : MonoBehaviour
         Debug.DrawRay(new Vector2(gameObject.transform.position.x + (-transform.localScale.x / 2), gameObject.transform.position.y + transform.localScale.y / 2), Vector2.up, Color.green);
         Debug.DrawRay(new Vector2(gameObject.transform.position.x + (transform.localScale.x / 2), gameObject.transform.position.y + transform.localScale.y / 2), Vector2.up, Color.green);
 
-
+        //Debug.Log(hitLeft.point.y - transform.position.y);
         if (hitLeft.collider != null || hitRight.collider != null)
         {
 
             if (hitLeft.point.y - transform.position.y > 1 && hitLeft.point.y - transform.position.y <= 1.6 || hitRight.point.y - transform.position.y > 1 && hitRight.point.y - transform.position.y <= 1.6)
             {
-                maxScale = 2;
+                maxScale = 1.95f;
 
             }
             if (hitLeft.point.y - transform.position.y > 0 && hitLeft.point.y - transform.position.y <= 1 || hitRight.point.y - transform.position.y >= 0 && hitRight.point.y - transform.position.y <= 1)
             {
-                maxScale = 1;
+                maxScale = 0.95f;
             }
-            else if (hitLeft.point.y - transform.position.y > 1.8 && hitRight.point.y - transform.position.y > 1.8)
+            if (hitLeft.point.y - transform.position.y > 1.5 && hitRight.point.y - transform.position.y > 1.5)
             {
-                maxScale = 3;
+                maxScale = 2.95f;
             }
 
         }
     }
     void WidthCheck()
     {
-        RaycastHit2D LowerHitLeft = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - transform.localScale.y / 2), Vector2.left);
-        RaycastHit2D LowerHitRight = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - transform.localScale.y / 2), Vector2.right);
+        //Alemmat castit
+        RaycastHit2D LowerHitLeft = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - transform.localScale.y / 2 + 0.1f), Vector2.left);
+        RaycastHit2D LowerHitRight = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - transform.localScale.y / 2 + 0.1f), Vector2.right);
 
-        RaycastHit2D upperHitLeft = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + transform.localScale.y / 2), Vector2.left);
-        RaycastHit2D upperHitRight = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + transform.localScale.y / 2), Vector2.right);
+        //Keskellä
+        RaycastHit2D middleHitLeft = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Vector2.left);
+        RaycastHit2D middleHitRight = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Vector2.right);
+
+        //Ylemmät
+        RaycastHit2D upperHitLeft = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + transform.localScale.y / 2 - 0.1f), Vector2.left);
+        RaycastHit2D upperHitRight = Physics2D.Raycast(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + transform.localScale.y / 2 - 0.1f), Vector2.right);
 
         Debug.DrawRay(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - transform.localScale.y / 2), Vector2.left, Color.blue);
         Debug.DrawRay(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - transform.localScale.y / 2), Vector2.right, Color.blue);
 
+        Debug.DrawRay(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Vector2.left, Color.blue);
+        Debug.DrawRay(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Vector2.right, Color.blue);
+
         Debug.DrawRay(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + transform.localScale.y / 2), Vector2.left, Color.blue);
         Debug.DrawRay(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + transform.localScale.y / 2), Vector2.right, Color.blue);
 
-        Debug.Log(LowerHitRight.point.x - LowerHitLeft.point.x);
-        Debug.Log(upperHitRight.point.x - upperHitLeft.point.x);
+        //Debug.Log(LowerHitRight.point.x - LowerHitLeft.point.x);
+        //Debug.Log(upperHitRight.point.x - upperHitLeft.point.x);
+        //Debug.Log(middleHitRight.point.x - middleHitLeft.point.x);
 
         if (LowerHitLeft.collider != null || LowerHitRight.collider != null)
         {
 
-            if (LowerHitRight.point.x - LowerHitLeft.point.x < 2.8 && LowerHitRight.point.x - LowerHitLeft.point.x >= 1.2 && upperHitRight.point.x - upperHitLeft.point.x < 2.8 && upperHitRight.point.x - upperHitLeft.point.x >= 1.2)
+            if (LowerHitRight.point.x - LowerHitLeft.point.x < 2.5 && LowerHitRight.point.x - LowerHitLeft.point.x > 1.4 || upperHitRight.point.x - upperHitLeft.point.x < 2.5 && upperHitRight.point.x - upperHitLeft.point.x > 1.4 || middleHitRight.point.x - middleHitLeft.point.x < 2.5 && middleHitRight.point.x - middleHitLeft.point.x > 1.4)
             {
-                maxWidthScale = 2;
+                maxWidthScale = 1.95f;
 
             }
-            if (LowerHitRight.point.x - LowerHitLeft.point.x < 2 && upperHitRight.point.x - upperHitLeft.point.x < 2)
+            if (LowerHitRight.point.x - LowerHitLeft.point.x < 1.1 || upperHitRight.point.x - upperHitLeft.point.x < 1.1 || middleHitRight.point.x - middleHitLeft.point.x < 1.1)
             {
-                maxWidthScale = 1;
+                maxWidthScale = 0.95f;
 
             }
-            if (LowerHitRight.point.x - LowerHitLeft.point.x > 2.8 && upperHitRight.point.x - upperHitLeft.point.x > 2.8)
+            if (LowerHitRight.point.x - LowerHitLeft.point.x > 2.5 && upperHitRight.point.x - upperHitLeft.point.x > 2.5 && middleHitRight.point.x - middleHitLeft.point.x > 2.5)
             {
-                maxWidthScale = 3;
+                maxWidthScale = 2.95f;
 
             }
 
